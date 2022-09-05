beb8c367bc77a5e17175601c002f86a773295fa4
Chee Kent Yong <cheekentyong@gmail.com>
2022-09-05 02:39:39 +0200

    Refactored to use proper memory addressing

--- vm/file_handling.go
+++ vm/file_handling.go
@@ -7,25 +7,18 @@ import (
 	"os"
 )
 
-func (vm *VirtualMachine) DumpMemory() [32768][]uint16 {
-	result := [32768][]uint16{}
+func (vm *VirtualMachine) DumpMemory() [32768]uint16 {
+	result := [32768]uint16{}
 	for address, value := range vm.memory {
-		result[address] = make([]uint16, len(value))
-		copy(result[address], value)
+		result[address] = value
 	}
 	return result
 }
 
 func (vm *VirtualMachine) load(file *os.File) error {
 	index := 0
-	var args []uint16
 	for {
-		op, err := readInt(file)
-
-		if op < 21 && len(vm.memory[index]) > 0 {
-			// This is an op that needs a new line before we can continue (to prevent rewriting old bytes)
-			index++
-		}
+		num, err := readInt(file)
 
 		if err != nil {
 			if err.Error() == "EOF" {
@@ -36,91 +29,9 @@ func (vm *VirtualMachine) load(file *os.File) error {
 			}
 		}
 
-		switch op {
-		case 0:
-			args = nil
-			break
-		case 1:
-			args, _ = readInts(file, 2)
-			break
-		case 2:
-			args, _ = readInts(file, 1)
-			break
-		case 3:
-			args, _ = readInts(file, 1)
-			break
-		case 4:
-			args, _ = readInts(file, 3)
-			break
-		case 5:
-			args, _ = readInts(file, 3)
-			break
-		case 6:
-			args, _ = readInts(file, 1)
-			break
-		case 7:
-			args, _ = readInts(file, 2)
-			break
-		case 8:
-			args, _ = readInts(file, 2)
-			break
-		case 9:
-			args, _ = readInts(file, 3)
-			break
-		case 10:
-			args, _ = readInts(file, 3)
-			break
-		case 11:
-			args, _ = readInts(file, 3)
-			break
-		case 12:
-			args, _ = readInts(file, 3)
-			break
-		case 13:
-			args, _ = readInts(file, 3)
-			break
-		case 14:
-			args, _ = readInts(file, 2)
-			break
-		case 15:
-			args, _ = readInts(file, 2)
-			break
-		case 16:
-			args, _ = readInts(file, 2)
-			break
-		case 17:
-			args, _ = readInts(file, 1)
-			break
-		case 18:
-			args = nil
-			break
-		case 19:
-			args, _ = readInts(file, 1)
-			break
-		case 20:
-			args, _ = readInts(file, 1)
-			break
-		case 21:
-			args = nil
-			break
-		default:
-			vm.memory[index] = append(vm.memory[index], op)
-			continue
-		}
-
-		vm.memory[index] = createOpSlice(op, args...)
+		vm.memory[index] = num
 		index++
 	}
-}
-
-func createOpSlice(op uint16, args ...uint16) []uint16 {
-	result := []uint16{op}
-
-	for _, arg := range args {
-		result = append(result, arg)
-	}
-
-	return result
 }
 
 // Reads and returns the identifier for the operation
@@ -135,20 +46,4 @@ func readInt(file *os.File) (uint16, error) {
 	instruction := binary.LittleEndian.Uint16(intBuff[:])
 
 	return instruction, nil
-}
-
-// Reads multiple integers and returns them as an array
-func readInts(file *os.File, n int) ([]uint16, error) {
-	ret := make([]uint16, n)
-	for i := 0; i < n; i++ {
-		result, err := readInt(file)
-
-		if err != nil {
-			return []uint16{}, err
-		}
-
-		ret[i] = result
-	}
-
-	return ret, nil
 }
--- vm/virtual_machine.go
+++ vm/virtual_machine.go
@@ -7,13 +7,14 @@ import (
 
 type VirtualMachine struct {
 	// 15 bit address space memory
-	memory [32768][]uint16
+	memory [32768]uint16
 	// Register set with 8 slots
 	register [8]uint16
 	// Unbounded stack
 	stack Stack
 	// Program counter
-	index uint16
+	index  uint16
+	opArgs map[uint16]uint16
 }
 
 type Stack struct {
@@ -26,9 +27,33 @@ func (stack *Stack) push(arg uint16) {
 
 func Load(file *os.File) (*VirtualMachine, error) {
 	vm := VirtualMachine{
-		memory:   [32768][]uint16{},
+		memory:   [32768]uint16{},
 		register: [8]uint16{},
 		stack:    Stack{inner: []uint16{}},
+		opArgs: map[uint16]uint16{
+			0:  0,
+			1:  2,
+			2:  1,
+			3:  1,
+			4:  3,
+			5:  3,
+			6:  1,
+			7:  2,
+			8:  2,
+			9:  3,
+			10: 3,
+			11: 3,
+			12: 3,
+			13: 3,
+			14: 2,
+			15: 2,
+			16: 2,
+			17: 1,
+			18: 0,
+			19: 1,
+			20: 1,
+			21: 0,
+		},
 	}
 	err := vm.load(file)
 
@@ -40,14 +65,17 @@ func Load(file *os.File) (*VirtualMachine, error) {
 }
 
 func (vm *VirtualMachine) Run() error {
+	defer func() {
+		fmt.Printf("Fault index: %v\n", vm.index)
+	}()
+
 	for {
-		op := vm.memory[vm.index][0]
+		op := vm.memory[vm.index]
 
 		// Prepare operands, get from registry if necessary
-		operands := make([]uint16, len(vm.memory[vm.index])-1)
-		for i, operand := range vm.memory[vm.index][1:] {
-			val := vm.checkRegister(operand)
-			operands[i] = val
+		var operands []uint16
+		if vm.opArgs[op] > 0 {
+			operands = vm.memory[vm.index+1 : vm.index+vm.opArgs[op]+1]
 		}
 
 		switch op {
@@ -55,6 +83,9 @@ func (vm *VirtualMachine) Run() error {
 			return nil
 		case 1:
 			vm.set(operands[0], operands[1])
+			break
+		case 2:
+			vm.push(operands[0])
 			break
 		case 6: // jmp
 			vm.jmp(operands[0])
@@ -84,9 +115,20 @@ func (vm *VirtualMachine) Run() error {
 	}
 }
 
-func (vm *VirtualMachine) checkRegister(arg uint16) uint16 {
+// returns the registry address, or the default value (passed in value) if arg is not a registry address.
+func tryGetRegistryAddress(arg uint16) (uint16, bool) {
 	if arg >= 32768 && arg <= 32775 { // Is within register values
-		index := arg - 32768
+		return arg - 32768, true
+	}
+
+	return arg, false
+}
+
+// returns the value of the registry, or the default value (passed in value) if arg does not point to a registry address.
+func (vm *VirtualMachine) tryGetRegistryValue(arg uint16) uint16 {
+	index, isRegistry := tryGetRegistryAddress(arg)
+
+	if isRegistry {
 		return vm.register[index]
 	}
 
@@ -95,46 +137,54 @@ func (vm *VirtualMachine) checkRegister(arg uint16) uint16 {
 
 // assign into <a> the sum of <b> and <c> (modulo 32768)
 func (vm *VirtualMachine) add(a uint16, b uint16, c uint16) {
-	vm.register[a] = (b + c) % 32768
-	vm.index++
+	index, _ := tryGetRegistryAddress(a)
+	vm.register[index] = (vm.tryGetRegistryValue(b) + vm.tryGetRegistryValue(c)) % 32768
+	vm.index += 4
+}
+
+func (vm *VirtualMachine) push(a uint16) {
+	vm.stack.push(vm.tryGetRegistryValue(a))
+	vm.index += 2
 }
 
 // set register <a> to the value of <b>
-func (vm *VirtualMachine) set(a uint16, b uint16) {
-	vm.register[a] = b
-	vm.index++
+func (vm *VirtualMachine) set(index uint16, b uint16) {
+	index, _ = tryGetRegistryAddress(index)
+	vm.register[index] = vm.tryGetRegistryValue(b)
+	vm.index += 3
 }
 
 // if <a> is nonzero, jump to <b>
 func (vm *VirtualMachine) jt(a uint16, b uint16) {
 	if a != 0 {
-		vm.jmp(b)
+		vm.jmp(vm.tryGetRegistryValue(b))
 		return
 	}
-	vm.index++
+	vm.index += 3
 }
 
 // if <a> is zero, jump to <b>. Returns the destination index
 func (vm *VirtualMachine) jf(a uint16, b uint16) {
 	if a == 0 {
-		vm.jmp(b)
+		vm.jmp(vm.tryGetRegistryValue(b))
 		return
 	}
-	vm.index++
+	vm.index += 3
 }
 
 func (vm *VirtualMachine) out(a uint16) {
-	fmt.Printf("%c", a)
-	vm.index++
+	fmt.Printf("%c", vm.tryGetRegistryValue(a))
+	vm.index += 2
 }
 
 // jump to <a>
 func (vm *VirtualMachine) jmp(a uint16) {
-	vm.index = a
+	vm.index = vm.tryGetRegistryValue(a)
+	fmt.Printf("jmp to %v\n", vm.index)
 }
 
 // write the address of the next instruction to the stack and jump to <a>
 func (vm *VirtualMachine) call(a uint16) {
-	vm.stack.push(vm.index + 1)
-	vm.jmp(a)
+	vm.stack.push(vm.index + 2)
+	vm.jmp(vm.tryGetRegistryValue(a))
 }
